cc61c0a618002253852052beeabea5ed42481281
caiomvital <caiomvital@gmail.com>
2024-09-03 07:44:15 -0300

    criado o método findAll com a CategoriaDTO

--- src/main/java/com/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/bookstore/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package com.bookstore.resources;
 
 import com.bookstore.domain.Categoria;
+import com.bookstore.dtos.CategoriaDTO;
 import com.bookstore.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -23,6 +27,14 @@ public class CategoriaResource {
     public ResponseEntity<Categoria> findById(@PathVariable Integer id){
         Categoria obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoriaDTO>> findAll(){
+        List<Categoria> list = service.findAll();
+        List<CategoriaDTO> listDTO = list.stream().map(CategoriaDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+
     }
 
 
