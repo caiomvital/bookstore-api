423240482b03af4ef12103955ebe0382aa47750e
caiomvital <caiomvital@gmail.com>
2024-09-02 23:21:46 -0300

    excecao personalizada para objeto nao encontrado

--- src/main/java/com/bookstore/service/CategoriaService.java
+++ src/main/java/com/bookstore/service/CategoriaService.java
@@ -2,6 +2,7 @@ package com.bookstore.service;
 
 import com.bookstore.domain.Categoria;
 import com.bookstore.repositories.CategoriaRepository;
+import com.bookstore.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,7 @@ public class CategoriaService {
     }
     public Categoria findById(Integer id){
         Optional<Categoria> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id + ", Tipo: " + Categoria.class.getName()));
 
     }
 }
