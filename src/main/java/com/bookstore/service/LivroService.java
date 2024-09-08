f20805c0124abb19f504ee7fad8416b37568d5bb
caiomvital <caiomvital@gmail.com>
2024-09-07 23:44:42 -0300

    criado o metodo atualizar livro

--- src/main/java/com/bookstore/service/LivroService.java
+++ src/main/java/com/bookstore/service/LivroService.java
@@ -32,4 +32,17 @@ public class LivroService {
         return repository.findAllByCategoria(id_cat);
 
     }
+
+    public Livro update(Integer id, Livro obj){
+        Livro newObj = findById(id);
+        updateData(newObj, obj);
+        return repository.save(newObj);
+    }
+
+    private void updateData(Livro newObj, Livro obj) {
+        newObj.setTitulo(obj.getTitulo());
+        newObj.setNomeAutor(obj.getNomeAutor());
+        newObj.setTexto(obj.getTexto());
+        newObj.setTexto(obj.getTexto());
+    }
 }
